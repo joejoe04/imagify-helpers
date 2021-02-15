f15b2994ad8bb3d75a824f0a498120a57d6bd3e5
joejoe04 <45705494+joejoe04@users.noreply.github.com>
2021-02-15 14:31:54 -0500

    Update imagify-keep-attributes-on-img-tags.php

--- webp/imagify-keep-attributes-on-img-tags/imagify-keep-attributes-on-img-tags.php
+++ webp/imagify-keep-attributes-on-img-tags/imagify-keep-attributes-on-img-tags.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name: Imagify | Keep Attributes On img Tags
- * Description: Keeps attributes on img tags rather than being moved to picture tags.
+ * Plugin Name: Imagify | Keep Attributes On &lt;img&gt; Tags
+ * Description: Keeps attributes on &lt;img&gt; tags rather than moving them to picture tags.
  * Plugin URI:  https://github.com/wp-media/imagify-helpers/tree/master/optimization/imagify-keep-attributes-on-img-tags/
- * Author: 			WP Media
+ * Author: 	WP Media
  * Author URI: 	https://wp-media.me/
- * Licence: 		GNU General Public License v2 or later
+ * Licence: 	GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * Copyright SAS WP Media 2020
